9ea943b4530dfbef83872d994aac70af0cd4c229
Vegard Øye <vegard_oye@proton.me>
2026-07-08 21:19:20 +0200

    Move test utilities into test-util.scm

--- test/scm/test-util.scm
+++ test/scm/test-util.scm
@@ -291,14 +291,86 @@
    (else
     exp)))
 
+(define (print-sexp exp)
+  (cond
+   ((undefined? exp)
+    "#u")
+   ((js/null? exp)
+    "#n")
+   ((boolean? exp)
+    (if exp "#t" "#f"))
+   ((cons? exp)
+    (string-append
+     "("
+     (string-join
+      (map print-sexp exp)
+      " ")
+     ")"))
+   ((string? exp)
+    (string-append
+     "\""
+     (~> exp
+         (regexp-replace (regexp "\\\\" "g") _ "\\\\")
+         (regexp-replace (regexp "\"" "g") _ "\\\""))
+     "\""))
+   ((symbol? exp)
+    (symbol->string exp))
+   (else
+    (string-append exp ""))))
+
+(defmacro test-macro (&rest body)
+  (define group '())
+  (define groups '())
+  (for ((i (range 0 (array-list-length body) 3)))
+    (define prompt
+      (aget body i))
+    (define expression
+      (aget body (+ i 1)))
+    (define value
+      (aget body (+ i 2)))
+    (cond
+     ((and (array-list? expression)
+           (= (array-list-length expression)
+              2)
+           (eq? (array-list-first expression)
+                'describe))
+      (when (> (array-list-length group) 0)
+        (push-right groups group)
+        (set! group '()))
+      (define description
+        (array-list-second expression))
+      (push-right! group description))
+     (else
+      (define it-description
+        (print-sexp expression))
+      (define it-expression
+        `(it ,it-description
+             (fn ()
+               (test-repl
+                '(roselisp
+                  ,prompt
+                  ,expression
+                  ,value)))))
+      (push-right! group it-expression))))
+  (when (> (array-list-length group) 0)
+    (push-right groups group))
+  (define tests
+    (map (lambda (group)
+           `(describe ,(first group)
+              (fn () ,@(rest group))))
+         groups))
+  `(begin ,@tests))
+
 (provide
+  (rename-out (test-repl test-repl-form))
+  (rename-out (test-repl test-shell-form))
   assert-equal
   assert-not-equal
   assert-throws
   compile-repl-form
   parse-repl-form
+  print-sexp
   simplify-repl-form
   test-lisp
-  test-repl
-  (rename-out (test-repl test-repl-form))
-  (rename-out (test-repl test-shell-form)))
+  test-macro
+  test-repl)
--- test/ts/test-util.ts
+++ test/ts/test-util.ts
@@ -826,15 +826,108 @@ function compileReplForm(exp: any, options: any = {}): any {
   }
 }
 
+function printSexp(exp: any): any {
+  if (exp === undefined) {
+    return '#u';
+  } else if (exp === null) {
+    return '#n';
+  } else if (typeof exp === 'boolean') {
+    if (exp) {
+      return '#t';
+    } else {
+      return '#f';
+    }
+  } else if (Array.isArray(exp) && !(Array.isArray(exp) && exp.length === 0)) {
+    return (
+      '(' +
+      exp
+        .map(function (x: any): any {
+          return printSexp(x);
+        })
+        .join(' ') +
+      ')'
+    );
+  } else if (typeof exp === 'string') {
+    return (
+      '"' +
+      exp
+        .replace(new RegExp('\\\\', 'g'), '\\\\')
+        .replace(new RegExp('"', 'g'), '\\"') +
+      '"'
+    );
+  } else if (typeof exp === 'symbol') {
+    return exp.description as string;
+  } else {
+    return exp + '';
+  }
+}
+
+function testMacro(exp: any, env: any): any {
+  const body: any = exp.slice(1);
+  let group: any = [];
+  const groups: any = [];
+  const _end: any = body.length;
+  for (let i: any = 0; i < _end; i = i + 3) {
+    const prompt: any = (body as any)[i];
+    const expression: any = body[i + 1];
+    const value: any = body[i + 2];
+    if (
+      Array.isArray(expression) &&
+      expression.length === 2 &&
+      expression[0] === Symbol.for('describe')
+    ) {
+      if (group.length > 0) {
+        groups.push(group);
+        group = [];
+      }
+      const description: any = expression[1];
+      group.push(description);
+    } else {
+      const itDescription: any = printSexp(expression);
+      const itExpression: any = [
+        Symbol.for('it'),
+        itDescription,
+        [
+          Symbol.for('fn'),
+          [],
+          [
+            Symbol.for('test-repl'),
+            [
+              Symbol.for('quote'),
+              [Symbol.for('roselisp'), prompt, expression, value],
+            ],
+          ],
+        ],
+      ];
+      group.push(itExpression);
+    }
+  }
+  if (group.length > 0) {
+    groups.push(group);
+  }
+  const tests: any = groups.map(function (group: any): any {
+    return [
+      Symbol.for('describe'),
+      group[0],
+      [Symbol.for('fn'), [], ...group.slice(1)],
+    ];
+  });
+  return [Symbol.for('begin'), ...tests];
+}
+
+testMacro.lispMacro = true;
+
 export {
+  testRepl as testReplForm,
+  testRepl as testShellForm,
   assertEqual,
   assertNotEqual,
   assertThrows,
   compileReplForm,
   parseReplForm,
+  printSexp,
   simplifyReplForm,
   testLisp,
+  testMacro,
   testRepl,
-  testRepl as testReplForm,
-  testRepl as testShellForm,
 };

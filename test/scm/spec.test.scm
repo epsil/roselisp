9f5bba6349a154d247a6c2790f6b0ec4d5b51209
Vegard Øye <vegard_oye@proton.me>
2026-07-08 22:49:15 +0200

    Add tests

--- test/scm/spec.test.scm
+++ test/scm/spec.test.scm
@@ -9,11 +9,101 @@
 (declare-macro test-macro)
 
 (test-macro
- ;; ;; license
- ;; > (describe "license")
- ;; _
- ;; > license
- ;; 'MPL-2.0
+ ;; `license`
+ > (describe "license")
+ _
+ > license
+ 'MPL-2.0
+
+ ;; `#t`
+ > (describe "#t")
+ _
+ > #t
+ #t
+ > '#t
+ #t
+
+ ;; `#f`
+ > (describe "#f")
+ _
+ > #f
+ #f
+ > '#f
+ #f
+
+ ;; `#u`
+ > (describe "#u")
+ _
+ > #u
+ #u
+ > '#u
+ #u
+ > undefined
+ #u
+ ;; > (undefined 0)
+ ;; #u
+ ;; > ((undefined 0) 0)
+ ;; #u
+
+ ;; `#n`
+ > (describe "#n")
+ _
+ > #n
+ #n
+ > '#n
+ #n
+ > js-null
+ #n
+ > js/null
+ #n
+
+ ;; `true?`
+ > (describe "true?")
+ _
+ > (true? #t)
+ #t
+ > (true? #f)
+ #f
+ > (true? #u)
+ #f
+ > (true? #n)
+ #f
+ > (true? '())
+ #t
+
+ ;; `false?`
+ > (describe "false?")
+ _
+ > (false? #t)
+ #f
+ > (false? #f)
+ #t
+ > (false? #u)
+ #t
+ > (false? #n)
+ #t
+ > (false? '())
+ #f
+
+ ;; `nil`
+ > (describe "nil")
+ _
+ > nil
+ '()
+ > (list? nil)
+ #t
+ > (length nil)
+ 0
+
+ ;; `null`
+ > (describe "null")
+ _
+ > null
+ '()
+ > (listp null)
+ #t
+ > (length null)
+ 0
 
  ;; `eq?`
  > (describe "eq?")
--- test/ts/spec.test.ts
+++ test/ts/spec.test.ts
@@ -6,6 +6,237 @@
 
 import { testRepl, testMacro } from './test-util';
 
+describe('license', function (): any {
+  return it('license', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      Symbol.for('license'),
+      [Symbol.for('quote'), Symbol.for('MPL-2.0')],
+    ]);
+  });
+});
+
+describe('#t', function (): any {
+  it('#t', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, true, true]);
+  });
+  return it('(quote #t)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('quote'), true],
+      true,
+    ]);
+  });
+});
+
+describe('#f', function (): any {
+  it('#f', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, false, false]);
+  });
+  return it('(quote #f)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('quote'), false],
+      false,
+    ]);
+  });
+});
+
+describe('#u', function (): any {
+  it('#u', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, undefined, undefined]);
+  });
+  it('(quote #u)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('quote'), undefined],
+      undefined,
+    ]);
+  });
+  return it('undefined', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      Symbol.for('undefined'),
+      undefined,
+    ]);
+  });
+});
+
+describe('#n', function (): any {
+  it('#n', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, null, null]);
+  });
+  it('(quote #n)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('quote'), null],
+      null,
+    ]);
+  });
+  it('js-null', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      Symbol.for('js-null'),
+      null,
+    ]);
+  });
+  return it('js/null', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      Symbol.for('js/null'),
+      null,
+    ]);
+  });
+});
+
+describe('true?', function (): any {
+  it('(true? #t)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('true?'), true],
+      true,
+    ]);
+  });
+  it('(true? #f)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('true?'), false],
+      false,
+    ]);
+  });
+  it('(true? #u)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('true?'), undefined],
+      false,
+    ]);
+  });
+  it('(true? #n)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('true?'), null],
+      false,
+    ]);
+  });
+  return it('(true? (quote ))', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('true?'), [Symbol.for('quote'), []]],
+      true,
+    ]);
+  });
+});
+
+describe('false?', function (): any {
+  it('(false? #t)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('false?'), true],
+      false,
+    ]);
+  });
+  it('(false? #f)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('false?'), false],
+      true,
+    ]);
+  });
+  it('(false? #u)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('false?'), undefined],
+      true,
+    ]);
+  });
+  it('(false? #n)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('false?'), null],
+      true,
+    ]);
+  });
+  return it('(false? (quote ))', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('false?'), [Symbol.for('quote'), []]],
+      false,
+    ]);
+  });
+});
+
+describe('nil', function (): any {
+  it('nil', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      Symbol.for('nil'),
+      [Symbol.for('quote'), []],
+    ]);
+  });
+  it('(list? nil)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('list?'), Symbol.for('nil')],
+      true,
+    ]);
+  });
+  return it('(length nil)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('length'), Symbol.for('nil')],
+      0,
+    ]);
+  });
+});
+
+describe('null', function (): any {
+  it('null', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      Symbol.for('null'),
+      [Symbol.for('quote'), []],
+    ]);
+  });
+  it('(listp null)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('listp'), Symbol.for('null')],
+      true,
+    ]);
+  });
+  return it('(length null)', function (): any {
+    return testRepl([
+      Symbol.for('roselisp'),
+      undefined,
+      [Symbol.for('length'), Symbol.for('null')],
+      0,
+    ]);
+  });
+});
+
 describe('eq?', function (): any {
   it('(eq? #t #t)', function (): any {
     return testRepl([

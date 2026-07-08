65760e48375f7c00193eac5df45f3c14708df215
Vegard Øye <vegard_oye@proton.me>
2026-07-08 21:30:13 +0200

    Merge roselisp.test.scm and spec.test.scm

--- test/ts/spec.test.ts
+++ test/ts/spec.test.ts
@@ -4,58 +4,88 @@
  * Tests expressed as a Roselisp REPL session.
  */
 
-import { testRepl, testMacro } from './test-util';
+import {
+  testRepl,
+  testMacro
+} from './test-util';
 
 describe('eq?', function (): any {
   it('(eq? #t #t)', function (): any {
-    return testRepl([
-      Symbol.for('roselisp'),
-      undefined,
-      [Symbol.for('eq?'), true, true],
-      true,
-    ]);
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('eq?'), true, true], true]);
   });
   it('(eq? #f #f)', function (): any {
-    return testRepl([
-      Symbol.for('roselisp'),
-      undefined,
-      [Symbol.for('eq?'), false, false],
-      true,
-    ]);
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('eq?'), false, false], true]);
   });
   return it('(eq? #t #f)', function (): any {
-    return testRepl([
-      Symbol.for('roselisp'),
-      undefined,
-      [Symbol.for('eq?'), true, false],
-      false,
-    ]);
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('eq?'), true, false], false]);
   });
 });
 
 describe('equal?', function (): any {
   it('(equal? #t #t)', function (): any {
-    return testRepl([
-      Symbol.for('roselisp'),
-      undefined,
-      [Symbol.for('equal?'), true, true],
-      true,
-    ]);
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('equal?'), true, true], true]);
   });
   it('(equal? #f #f)', function (): any {
-    return testRepl([
-      Symbol.for('roselisp'),
-      undefined,
-      [Symbol.for('equal?'), false, false],
-      true,
-    ]);
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('equal?'), false, false], true]);
   });
   return it('(equal? #t #f)', function (): any {
-    return testRepl([
-      Symbol.for('roselisp'),
-      undefined,
-      [Symbol.for('equal?'), true, false],
-      false,
-    ]);
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('equal?'), true, false], false]);
+  });
+});
+
+describe('+', function (): any {
+  it('(+)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('+')], 0]);
+  });
+  it('(+ 1)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('+'), 1], 1]);
+  });
+  it('(+ 1 2)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('+'), 1, 2], 3]);
+  });
+  return it('(+ 1 2 4)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('+'), 1, 2, 4], 7]);
+  });
+});
+
+describe('-', function (): any {
+  it('(-)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('-')], 0]);
+  });
+  it('(- 1)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('-'), 1], -1]);
+  });
+  it('(- 1 2)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('-'), 1, 2], -1]);
+  });
+  return it('(- 1 2 4)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('-'), 1, 2, 4], -5]);
+  });
+});
+
+describe('*', function (): any {
+  it('(*)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('*')], 1]);
+  });
+  it('(* 1)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('*'), 1], 1]);
+  });
+  it('(* 1 2)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('*'), 1, 2], 2]);
+  });
+  return it('(* 1 2 4)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('*'), 1, 2, 4], 8]);
+  });
+});
+
+describe('/', function (): any {
+  it('(/ 1)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('/'), 1], 1]);
+  });
+  it('(/ 1 2)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('/'), 1, 2], 0.5]);
+  });
+  return it('(/ 1 2 4)', function (): any {
+    return testRepl([Symbol.for('roselisp'), undefined, [Symbol.for('/'), 1, 2, 4], 0.125]);
   });
 });
